3fbfecca841404e6cfef28da5adc4ba32bf37409
Fritz <fritz@junoservices.com>
2024-01-28 09:09:42 +1300

    Refined Treatment Selection methods
    Made the treatment selection mode more consistent wrt underscores versus dashes
    for "bca-egal", "bca-ibcr" (previously "bca_egal", "bca_ibcr"). For backward
    compatibility, both conventions will now work.

--- MLModelClasses/obj/Debug/net8.0/MLModelClasses.AssemblyInfo.cs
+++ MLModelClasses/obj/Debug/net8.0/MLModelClasses.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MLModelClasses")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64a973f81225099d888bd8be3a968df59da40cb8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8da07dc76ee8c0c6c655308ab5d243becfda1ca1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MLModelClasses")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MLModelClasses")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
