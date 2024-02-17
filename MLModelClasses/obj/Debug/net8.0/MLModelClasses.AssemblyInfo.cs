321d6fe655296a05368e9bed882e53c889393453
Fritz <fritz@junoservices.com>
2024-02-17 16:43:29 +1300

    Added Network Functions in model setup

--- MLModelClasses/obj/Debug/net8.0/MLModelClasses.AssemblyInfo.cs
+++ MLModelClasses/obj/Debug/net8.0/MLModelClasses.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MLModelClasses")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8da07dc76ee8c0c6c655308ab5d243becfda1ca1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fbfecca841404e6cfef28da5adc4ba32bf37409")]
 [assembly: System.Reflection.AssemblyProductAttribute("MLModelClasses")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MLModelClasses")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
