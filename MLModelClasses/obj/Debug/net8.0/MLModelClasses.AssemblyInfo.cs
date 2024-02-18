2d97a405af98a4a450a3ea666b4d923a80517503
Fritz <fritz@junoservices.com>
2024-02-18 13:25:00 +1300

    Added 'budget_category' as output field for treatments.

--- MLModelClasses/obj/Debug/net8.0/MLModelClasses.AssemblyInfo.cs
+++ MLModelClasses/obj/Debug/net8.0/MLModelClasses.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MLModelClasses")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fbfecca841404e6cfef28da5adc4ba32bf37409")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+321d6fe655296a05368e9bed882e53c889393453")]
 [assembly: System.Reflection.AssemblyProductAttribute("MLModelClasses")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MLModelClasses")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
